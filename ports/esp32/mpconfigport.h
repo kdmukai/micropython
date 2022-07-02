01b191654999f0c99ea6105a02157f847b5b087a
kdmukai <keith.mukai@essaytagger.com>
2022-07-02 15:02:19 -0500

    Customizing esp32 to enable secp256k1 (requires additional library from Bitcoin Core to be compiled in) and hashlib.

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -200,6 +200,10 @@
 #define MICROPY_PY_BLUETOOTH_RANDOM_ADDR    (1)
 #define MICROPY_PY_BLUETOOTH_DEFAULT_GAP_NAME ("ESP32")
 
+// kdmukai custom firmware
+#define MODULE_SECP256K1_ENABLED (1)
+#define MODULE_HASHLIB_ENABLED (1)
+
 // fatfs configuration
 #define MICROPY_FATFS_ENABLE_LFN            (1)
 #define MICROPY_FATFS_RPATH                 (2)
